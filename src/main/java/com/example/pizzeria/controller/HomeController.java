a417ee27e41858ce54aaa17192580cca1e161081
praca praca <valdyrhjord@gmail.com>
2021-09-13 12:39:51 +0200

    Problem solving in HomeController and index.html

--- src/main/java/com/example/pizzeria/controller/HomeController.java
+++ src/main/java/com/example/pizzeria/controller/HomeController.java
@@ -1,24 +1,27 @@
 package com.example.pizzeria.controller;
 
 import com.example.pizzeria.model.Pizza;
+import com.example.pizzeria.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 import java.util.List;
 
 @Controller()
 public class HomeController {
 
-    private final PizzaController pizzaController;
+    private final PizzaService pizzaService;
 
-    public HomeController(PizzaController pizzaController) {
-        this.pizzaController = pizzaController;
+    @Autowired
+    public HomeController(PizzaService pizzaService) {
+        this.pizzaService = pizzaService;
     }
 
-    public String goHome(){
+
+    public String goHome(Model model){
+        model.addAttribute(String.valueOf(pizzaService.pizzaList()),pizzaService);
         return "index";
     }
 
-    public List<Pizza> getAll(){
-       return pizzaController.getAllPizzas();
-    }
 }
