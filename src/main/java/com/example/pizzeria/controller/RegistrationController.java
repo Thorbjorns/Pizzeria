2ff874a8b4efb463199964ab6ae0aa87f7433c65
praca praca <valdyrhjord@gmail.com>
2021-09-21 16:30:11 +0200

    signup view

--- src/main/java/com/example/pizzeria/controller/RegistrationController.java
+++ src/main/java/com/example/pizzeria/controller/RegistrationController.java
@@ -2,6 +2,7 @@ package com.example.pizzeria.controller;
 
 import com.example.pizzeria.security.userApp.RegistrationRequest;
 import com.example.pizzeria.service.RegistrationService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(path = "api/v1/registration")
 public class RegistrationController {
 
@@ -17,6 +18,11 @@ public class RegistrationController {
 
     public RegistrationController(RegistrationService registrationService) {
         this.registrationService = registrationService;
+    }
+
+    @GetMapping
+    public String getSignUpView(){
+        return "signup";
     }
 
     @PostMapping
